e8180699c95133b28f867a670b8ec18bafc04a09
sixoseven <mdcbowen@gmail.com>
2025-09-15 14:35:00 -0700

    ok

--- ch1/echo1.go
+++ ch1/echo1.go
@@ -1,18 +1,17 @@
-// Echo
+// Echo prints its command-line arguments (one would think)
 
 package main
 
 import (
 	"fmt"
 	"os"
-
 )
 
 func main() {
-    var s, sep string
-    for i := 1; i < len(os.Args); i++ {
-        s += sep + os.Args[i]
-        sep = " "
-    }
-    fmt.Println(s)
+	var s, sep string
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
 }
