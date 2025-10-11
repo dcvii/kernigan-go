3ddae68c0d8a92039c90fa38c88cb766db7b10d7
sixoseven <mdcbowen@gmail.com>
2025-10-11 15:20:44 -0700

    now going to skip

--- ch2/boiling/main.go
+++ ch2/boiling/main.go
@@ -12,6 +12,16 @@ func main() {
 	fmt.Println("Hello, World!")
 	var f = boilingF
 	var c = FToC(f)
-	fmt.Printf("Freezing = %g F or %g C", f, c)
+	fmt.Printf("Freezing = %g F or %g C\n", f, c)
+
+	medals := []string{
+		"Gold",
+		"Silver",
+		"Bronze",
+	}
+
+	for i, medal := range medals {
+		fmt.Printf("Medal %d: %s\n", i+1, medal)
+	}
 
 }
