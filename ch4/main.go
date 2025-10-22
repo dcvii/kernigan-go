25cdcc6c5b1eb9614b8606be0cafbbdcb25f0054
mdcb607 <mdcbowen@gmail.com>
2025-10-21 17:36:39 -0700

    finally

---
+++ ch4/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+type Employee struct {
+	ID        int
+	Name      string
+	Address   string
+	DoB       string
+	Position  string
+	Salary    int
+	ManagerID int
+}
+
+func EmployeeByID(id int) *Employee { /* */
+	return &Employee{}
+}
+
+func main() {
+
+	var dilbert Employee
+
+	dilbert.Salary = 100000
+	position := "Software Engineer"
+	dilbert.Position = position
+
+	{
+		position := &dilbert.Position
+		*position = "Senior " + *position
+	}
+
+	var employeeOfTheMonth *Employee = &dilbert
+	employeeOfTheMonth.Position += " (proactive team player)"
+
+	fmt.Println(EmployeeByID(dilbert.ManagerID).Position) // PHB
+
+	id := dilbert.ID
+	EmployeeByID(id).Salary = 0
+
+	fmt.Println(employeeOfTheMonth.Position)
+}
